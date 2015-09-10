85b37415986d6698178df279cbcbeca36ca7765d
22a <meehanp2@tcd.ie>
2015-09-10 18:41:48 +0100

    added ducss logos, nigel

--- www/js/ctrl-signup.js
+++ www/js/ctrl-signup.js
@@ -5,7 +5,7 @@ angular.module('starter.controllers')
 
   var hash = function(s){
     return s.split("").reduce(function(a,b){a=((a<<5)-a)+b.charCodeAt(0);return a&a},0);
-  }
+  };
 
   $scope.signup = function (user) {
     //buffered local storage
